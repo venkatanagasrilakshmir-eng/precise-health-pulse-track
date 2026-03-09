1e2ceed91f20c9299c0a8509efccf5995e8133a7
venkatanagasrilakshmir-eng <venkatanagasrilakshmir@gmail.com>
2026-03-09 10:18:48 +0530

    Create mongo.js

---
+++ database/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/healthpulse", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Database Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
